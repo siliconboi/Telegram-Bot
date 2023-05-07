ff7ee688e9ce6e11fa7fb350a2b21ca67646397c
Aishwary Tripathi <aishwarytripathi2001@gmail.com>
2023-05-07 22:39:35 +0530

    fix

--- backend/bot.js
+++ backend/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "5613288658:AAHIUkWOjTzvvxQREDfjfqOuB7LnNCsnxa0";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "........";
+const web_link = "https://6457d9f81929ef7e24e068da--unique-manatee-a88729.netlify.app/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome to Brother's Cafe", {
